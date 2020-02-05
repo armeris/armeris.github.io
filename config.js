f3353431e8d44ec91ca8bad62a0c24000eb6c5ff
Rubén <rubensalinasgarcia@protonmail.com>
2020-02-05 21:00:07 +0100

    Added author

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteTitle: 'Boda Laura & Rubén', // <title>
   pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
-  // social
+    // social
+  authorName: 'Rubén Salinas',
 
   pageTitle: 'Boda Laura & Rubén',
   weddingDate: '16 de Mayo de 2020',
