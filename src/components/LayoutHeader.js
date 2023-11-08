ebe3e378c73ce6c3f8d8c9e8bc05d18ffad01297
Rubén Salinas <ruben.salinas@edreamsodigeo.com>
2023-11-08 09:52:49 +0100

    [INVITACION] Clean old code

--- src/components/LayoutHeader.js
+++ src/components/LayoutHeader.js
@@ -1,42 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StaticQuery, graphql } from 'gatsby';
-import Helmet from 'react-helmet';
 import '../assets/sass/main.scss';
-const LayoutHeader = ({ children, darkText }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet title={data.site.siteMetadata.title}>
-          <html lang="es" />
-          <meta property="og:title" content="Boda Laura & Rubén" />
-          <meta property="og:description" content="17 de Octubre de 2020" />
-          <meta property="og:url" content="https://www.bodalaurayruben.com" />
-          <meta
-            property="og:image"
-            content="https://www.bodalaurayruben.com/static/invi_back-ca50c893347ce30d262e75a0cc48d5e3.jpg"
-          />
-        </Helmet>
-        <div id="wrapper-header" className={darkText ? 'dark-text' : ''}>
-          {children}
-        </div>
-      </>
-    )}
-  />
-);
-
-LayoutHeader.propTypes = {
-  children: PropTypes.node.isRequired,
-  darkText: PropTypes.bool,
+const LayoutHeader = ({ children, darkText }) => {
+        return (
+                <>
+                        <div id="wrapper-header" className={darkText ? 'dark-text' : ''}>
+                                {children}
+                        </div>
+                </>
+        );
 };
 
+LayoutHeader.propTypes = {
+        children: PropTypes.node.isRequired,
+        darkText: PropTypes.bool,
+};
+export const Head = () => {
+        <>
+                <html lang="es" />
+                <meta property="og:title" content="Boda Laura & Rubén" />
+                <meta property="og:description" content="17 de Octubre de 2020" />
+                <meta property="og:url" content="https://www.bodalaurayruben.com" />
+                <meta
+                        property="og:image"
+                        content="https://www.bodalaurayruben.com/static/invi_back-ca50c893347ce30d262e75a0cc48d5e3.jpg"
+                />
+        </>
+}
 export default LayoutHeader;
